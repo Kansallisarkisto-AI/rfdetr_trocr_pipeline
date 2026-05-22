c1e4b70d55a8efc9500072e898f1a62e703c4eed
John Mäkelä <29544613+woroko@users.noreply.github.com>
2026-05-22 15:25:21 +0300

    some updates for sfrd

--- nafhtr/__init__.py
+++ nafhtr/__init__.py
@@ -1,6 +1,6 @@
 from .main import load_rfdetr_model, load_trocr_model, get_text_predictions, TextPredictionInput
 from .seg_inference import predict_polygons
-from .utils import get_default_region, get_line_regions, order_regions_lines
+from .utils import get_default_region, get_line_regions, order_regions_lines, get_iou
 
 __all__ = [
     "load_rfdetr_model",
@@ -11,4 +11,5 @@ __all__ = [
     "get_default_region",
     "get_line_regions",
     "order_regions_lines",
+    "get_iou"
 ]
